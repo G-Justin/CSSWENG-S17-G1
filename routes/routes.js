005a894809dd090545ee84ef017948217c17ad3c
Justin <justingalura@gmail.com>
2020-07-28 20:35:36 +0800

    Revert "dependencies and general setup"
    This reverts commit 724de9d384f7d863a062263b84b4f3faf93e4f96.

--- routes/routes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const app = express();
-const loginController = require('../controller/loginController.js');
-
-app.get('/admin', loginController.getLogin);
-
-module.exports = app;
