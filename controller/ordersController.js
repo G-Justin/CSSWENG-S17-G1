7d848e2bc7952588abad536aa5377752d5bba7b3
Justin <justingalura@gmail.com>
2020-08-07 04:45:36 +0800

    order search by id

--- controller/ordersController.js
+++ controller/ordersController.js
@@ -6,6 +6,13 @@ const ordersController = {
             res.redirect('/login');
             return;
         }
+        
+
+        if (req.query.orderId != null) {
+            let orderId = req.query.orderId;
+            res.redirect('/admin/orders/' + orderId.trim());
+            return;
+        }
 
         Order.find({})
         .select('_id firstname lastname paymentStatus deliveryStatus')
