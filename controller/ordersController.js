8efa511ed2ee7f4939c7faa85dd964b94c619d05
Justin <justingalura@gmail.com>
2020-08-20 23:06:39 +0800

    pagination functionality

--- controller/ordersController.js
+++ controller/ordersController.js
@@ -17,14 +17,14 @@ const ordersController = {
         let paymentQueries = new Array();
 
         let deliveryStatus = sanitize(req.query.deliveryStatus);
-        if (deliveryStatus == 'SELECT' || deliveryStatus == null) {
+        if (deliveryStatus == 'SELECT' || deliveryStatus == null || deliveryStatus == 'undefined') {
             deliveryQueries.push('PROCESSING', 'DELIVERING', 'DELIVERED');
         } else {
             deliveryQueries.push(deliveryStatus);
         }
 
         let paymentStatus = sanitize(req.query.paymentStatus);
-        if (paymentStatus == 'SELECT' || paymentStatus == null) {
+        if (paymentStatus == 'SELECT' || paymentStatus == null || paymentStatus == 'undefined') {
             paymentQueries.push('TO PAY', 'PAID');
         } else {
             paymentQueries.push(paymentStatus);
@@ -34,54 +34,67 @@ const ordersController = {
         let dateEnd = parseDate(sanitize(req.query.dateEnd));
         let hasDateQuery = dateStart != null && dateEnd != null;
 
-        let page = Number(sanitize(req.query.page));
+        let page = sanitize(req.query.page);
+        console.log("before " + page)
         if (page == null) {
             page = 1;
         }
+        console.log("after " + page)
 
-        Order.find({
+        let options = {
+            select: CARD_SELECT,
+            lean: true,
+            page: page,
+            limit: 2
+        };
+
+        dateStart = (dateStart == null) ? new Date(-8640000000000000) : dateStart;
+        dateEnd = (dateEnd == null) ? new Date(8640000000000000) : dateEnd;
+        console.log(dateStart.toISOString());
+        console.log(dateEnd.toISOString());
+
+        let query= {
             deliveryStatus: {$in: deliveryQueries},
-            paymentStatus: {$in : paymentQueries}
-        })
-        .select(CARD_SELECT)
-        .skip((page - 1) * 2)
-        .limit(2)
-        .lean()
-        .exec(function(err, results) {
+            paymentStatus: {$in : paymentQueries},
+            
+            
+            orderDate: {
+                $gte: dateStart.toISOString(),
+                $lte: dateEnd.toISOString()
+            } 
+        };
+
+        Order.paginate(query, options, 
+            function(err, results) {
             let filteredOrders = new Array();
+            console.log(results)
            
-            for (let i = 0; i < results.length; i++) {
-                if (hasDateQuery) {
-                    let dateCopy = results[i].orderDate;
-                    dateCopy.setHours(12, 0, 0, 0);
-
-                    if (dateCopy >= dateStart && dateCopy <= dateEnd) {
-                        filteredOrders.push(results[i]);
-                    }
-                } else {
-                    filteredOrders.push(results[i]);
-                }
-                
-            }
-
             let selectOptions = new Array();
-            for (let i = 0; i < Math.ceil(filteredOrders.length / 2); i++) {
+            for (let i = 0; i < results.totalPages; i++) {
                 let no = i + 1;
                 let options = {
                     pageLink: "/admin/orders?deliveryStatus=" + deliveryStatus + "&paymentStatus=" + paymentStatus + "&dateStart=" + req.query.dateStart + "&dateEnd=" + req.query.dateEnd + "&page=" + no,
-                    pageNo: no
+                    pageNo: no,
+                    isSelected: (results.page == no),
                 };
 
                 selectOptions.push(options);
             }
 
+            let prevPageLink = results.hasPrevPage ? "/admin/orders?deliveryStatus=" + deliveryStatus + "&paymentStatus=" + paymentStatus + "&dateStart=" + req.query.dateStart + "&dateEnd=" + req.query.dateEnd + "&page=" + results.prevPage : ""
+            let nextPageLink = results.hasNextPage ? "/admin/orders?deliveryStatus=" + deliveryStatus + "&paymentStatus=" + paymentStatus + "&dateStart=" + req.query.dateStart + "&dateEnd=" + req.query.dateEnd + "&page=" + results.nextPage : ""
             res.render('admin/orders', {
                 title: 'Facemustph | Orders',
                 layout: 'main',
 
-                orderCards: filteredOrders,
-                selectOptions: selectOptions
-            });  
+                orderCards: results.docs,
+                selectOptions: selectOptions,
+                hasPrev: results.hasPrevPage,
+                hasNext: results.hasNextPage,
+                prevPageLink: prevPageLink,
+                nextPageLink: nextPageLink
+
+            });
         })
     }
 }
@@ -91,12 +104,13 @@ function parseDate(s) {
         return null;
     }
 
-    if (s == null) {
+    if (s == null || s === undefined) {
         return null;
     }
     var b = s.split(/\D/);
     let date = new Date(b[0], --b[1], b[2]);
-    date.setHours(12, 0, 0, 0);
+
+    date.setHours(8, 0, 0, 0);
 
     return date;
 }
