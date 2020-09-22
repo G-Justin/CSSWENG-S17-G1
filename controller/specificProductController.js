47ef08dfde84753afa2ec7bddbe90172296fcc4f
Justin <justingalura@gmail.com>
2020-09-23 02:57:56 +0800

    cleanup of specificProductController

--- controller/specificProductController.js
+++ controller/specificProductController.js
@@ -3,18 +3,6 @@ const sanitize = require('mongo-sanitize');
 const Product = require('../model/product.js');
 
 const specificProductController = {
-    getDashboard: function(req, res) {
-        if (!(req.session.user && req.cookies.user_sid)) {
-            res.redirect('/login');
-            return;
-        }
-
-        res.render('admin/dashboard', 
-        {
-            title: 'Admin Dashboard'
-        });
-    },
-
     getSpecificProduct: function(req, res) {
         if (req.session.cart === null || req.session.cart === undefined) {
             req.session.cart = new Array();
