9160315076522ddbc868529ca246139604305312
Justin <justingalura@gmail.com>
2020-09-30 02:54:09 +0800

    order page

--- controller/ordersController.js
+++ controller/ordersController.js
@@ -1,5 +1,7 @@
 const moment = require('moment');
 const Order = require('../model/order.js');
+const OrderItem = require('../model/orderitem.js');
+const Product = require('../model/product.js')
 const sanitize = require('mongo-sanitize');
 const { filter } = require('async');
 const { options } = require('../routes/routes.js');
@@ -20,110 +22,100 @@ const ordersController = {
             return;
         }
         
-
-        let deliveryQueries = new Array();
-        let paymentQueries = new Array();
-
-        let deliveryStatus = sanitize(req.query.deliveryStatus);
-        if (deliveryStatus == 'SELECT' || deliveryStatus == null || deliveryStatus == 'undefined') {
-            deliveryQueries.push('PROCESSING', 'DELIVERED');
-        } else {
-            deliveryQueries.push(deliveryStatus);
-        }
-
-        let paymentStatus = sanitize(req.query.paymentStatus);
-        if (paymentStatus == 'SELECT' || paymentStatus == null || paymentStatus == 'undefined') {
-            paymentQueries.push('TO PAY', 'PAID');
-        } else {
-            paymentQueries.push(paymentStatus);
-        }
-
         let dateStart = parseDate(sanitize(req.query.dateStart));
         let dateEnd = parseDate(sanitize(req.query.dateEnd));
-        let resultsMessage = getResultsMessage(deliveryStatus, paymentStatus, dateStart, dateEnd)
+        let resultsMessage = getResultsMessage(dateStart, dateEnd)
         
-        let page = sanitize(req.query.page);
-        if (page == null) {
-            page = 1;
-        }
-
-        let options = {
-            select: CARD_SELECT,
-            lean: true,
-            page: page,
-            limit: 3,
-
-            sort: {
-                orderDate: -1
-            }
-        };
-
         dateStart = (dateStart == null) ? new Date(-8640000000000000) : dateStart;
         dateEnd = (dateEnd == null) ? new Date(8640000000000000) : dateEnd;
 
-        
         let query= {
-            deliveryStatus: {$in: deliveryQueries},
-            paymentStatus: {$in : paymentQueries},
-            
-            
             orderDate: {
                 $gte: dateStart.toISOString(),
                 $lte: dateEnd.toISOString()
             } 
         };
 
+        Order.find(query)
+            .sort({orderDate: -1})
+            .exec((err, ordersResult) => {
+                if (err) {
+                    console.log('orderscontroller.js: err on querying for the orders')
+                    res.render('error', {
+                        title: 'Facemust',
+                        error: '404',
+                        message: 'AN ERROR OCCURRED'
+                    })
+                    return;
+                }
+                
+                let orders = new Array();
+                getOrders(ordersResult, orders).then((a) => {
+                    res.render('admin/orders', {
+                        title: 'Facemustph | Orders',
+                        layout: 'main',
+                        resultsMessage: resultsMessage,
         
-        Order.paginate(query, options, 
-            function(err, results) {
-            let filteredOrders = new Array();
-           
-            let selectOptions = new Array();
-            for (let i = 0; i < results.totalPages; i++) {
-                let no = i + 1;
-                let options = {
-                    pageLink: "/admin/orders?deliveryStatus=" + deliveryStatus + "&paymentStatus=" + paymentStatus + "&dateStart=" + req.query.dateStart + "&dateEnd=" + req.query.dateEnd + "&page=" + no,
-                    pageNo: no,
-                    isSelected: (results.page == no),
-                };
-
-                selectOptions.push(options);
-            }
-
-            let prevPageLink = results.hasPrevPage ? "/admin/orders?deliveryStatus=" + deliveryStatus + "&paymentStatus=" + paymentStatus + "&dateStart=" + req.query.dateStart + "&dateEnd=" + req.query.dateEnd + "&page=" + results.prevPage : ""
-            let nextPageLink = results.hasNextPage ? "/admin/orders?deliveryStatus=" + deliveryStatus + "&paymentStatus=" + paymentStatus + "&dateStart=" + req.query.dateStart + "&dateEnd=" + req.query.dateEnd + "&page=" + results.nextPage : ""
-            res.render('admin/orders', {
-                title: 'Facemustph | Orders',
-                layout: 'main',
-                resultsMessage: resultsMessage,
-
-                orderCards: results.docs,
-                selectOptions: selectOptions,
-                hasPrev: results.hasPrevPage,
-                hasNext: results.hasNextPage,
-                prevPageLink: prevPageLink,
-                nextPageLink: nextPageLink
-
-            });
-        })
+                        orderCards: orders
+        
+                    });
+                })
+            })
     }
 }
 
-function getResultsMessage(deliveryStatus, paymentStatus, dateStart, dateEnd) {
-    let deliveryMsg;
-    if (deliveryStatus == 'SELECT' || deliveryStatus == null || deliveryStatus == 'undefined') {
-        deliveryMsg = "";
-    } else {
-        deliveryMsg = deliveryStatus;
-    }
+async function checkDeficit(orderResultItems, d) {
+    for (let i = 0; i < orderResultItems.length; i++) {
+        let productResult = await Product.findOne(orderResultItems[i].product);
+        let smallDeficit = productResult.smallAvailable < orderResultItems[i].smallAmount ? (orderResultItems[i].smallAmount - productResult.smallAvailable) : 0;
+        let mediumDeficit = productResult.mediumAvailable < orderResultItems[i].mediumAmount ? (orderResultItems[i].mediumAmount - productResult.mediumAvailable) : 0;
+        let largeDeficit = productResult.largeAvailable < orderResultItems[i].largeAmount ? (orderResultItems[i].largeAmount - productResult.largeAvailable) : 0;
+        let extraLargeDeficit = productResult.extraLargeAvailable < orderResultItems[i].extraLargeAmount ? (orderResultItems[i].extraLargeAmount - productResult.extraLargeAvailable) : 0;
 
-    let paymentMsg;
-    if (paymentStatus == 'SELECT' || paymentStatus == null || paymentStatus == 'undefined') {
-        paymentMsg = "";
-    } else {
-        paymentMsg = paymentStatus;
+        if (smallDeficit > 0 || mediumDeficit > 0 || largeDeficit > 0 || extraLargeDeficit > 0) {
+            d.push(1);
+            return;
+        }
+    
     }
+}
 
+async function getOrders(ordersResult, orders) {
+    for (let i = 0; i < ordersResult.length; i++) {
+        let order = {
+            _id: ordersResult[i]._id,
+            firstname: ordersResult[i].firstname,
+            lastname: ordersResult[i].lastname,
+            paymentStatus: ordersResult[i].paymentStatus,
+            deliveryStatus: ordersResult[i].deliveryStatus,
+            hasDeficit: false
+        }
+
+        if(order.deliveryStatus == 'PROCESSING') {
+            let orderItems = await OrderItem.find({parentOrder: order._id});
+            for (let j = 0; j < orderItems.length; j++) {
+                let product = await Product.findOne({_id: orderItems[j].product});
+                if (!product) {
+                    continue;
+                }
+
+                let smallDeficit = product.smallAvailable < orderItems[j].smallAmount ? (orderItems[j].smallAmount - product.smallAvailable) : 0;
+                let mediumDeficit = product.mediumAvailable < orderItems[j].mediumAmount ? (orderItems[j].mediumAmount - product.mediumAvailable) : 0;
+                let largeDeficit = product.largeAvailable < orderItems[j].largeAmount ? (orderItems[j].largeAmount - product.largeAvailable) : 0;
+                let extraLargeDeficit = product.extraLargeAvailable < orderItems[j].extraLargeAmount ? (orderItems[j].extraLargeAmount - product.extraLargeAvailable) : 0;
+
+                if (smallDeficit > 0 || mediumDeficit > 0 || largeDeficit > 0 || extraLargeDeficit > 0) {
+                    order.hasDeficit = true;
+                    break;
+                } 
+            }
+        }
+
+        orders.push(order);
+    }
+}
+
+function getResultsMessage(dateStart, dateEnd) {
     let dateMsg;
     if (dateStart == null && dateEnd == null ) {
         dateMsg = "";
@@ -136,7 +128,7 @@ function getResultsMessage(deliveryStatus, paymentStatus, dateStart, dateEnd) {
     }
 
 
-    return deliveryMsg + " " + paymentMsg + " " + dateMsg;
+    return dateMsg;
 }
 
 function formatDate(dt) {
